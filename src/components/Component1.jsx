9111b57c73ac210ea391ccbae8e6399b83a07adf
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2026-06-03 00:03:32 +0530

    React: Introduction to React, Setup, JSX, Components, Props

---
+++ src/components/Component1.jsx
@@ -0,0 +1,10 @@
+function Component1() {
+  return (
+    <div className='Component1'>
+      <h1>Component 1</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit aspernatur possimus excepturi assumenda, labore dolorum necessitatibus tempora aliquid nostrum, quidem quae repellendus nemo sapiente! Autem deleniti iste unde voluptatibus totam!</p>
+    </div>
+  )
+}
+
+export default Component1
